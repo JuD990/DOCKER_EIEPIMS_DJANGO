aef4db7d8dac826e554e6a7d8aad09bcd929d17c
Jude Adolfo <judea3264@gmail.com>
2025-06-28 09:05:59 +0800

    Flatten frontend: remove submodule and add as regular directory

---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Welcome() {
+  return (
+    <div>
+      <h2>Welcome Component</h2>
+      <p>This is a custom React component!</p>
+    </div>
+  );
+}
+
+export default Welcome;
